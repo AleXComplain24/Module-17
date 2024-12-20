8173b5c6c41c18e9e32546eb43a1ea5011018c30
AleXComplain24 <alexkorg98@gmail.com>
2024-12-20 11:10:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from routers import category
+from routers import task, user
+import uvicorn
+
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    return {"message": "My shop"}
+
+# Подключение маршрутов
+app.include_router(task.router)
+app.include_router(user.router)
+
+# Запуск приложения
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
